29db1536a95fbf0f895fa91999d762077244d020
Peter <klennepette@gmail.com>
2026-07-23 12:10:09 +0200

    chore: remove .svelte-kit artifacts, update .gitignore

--- .svelte-kit/output/client/_app/immutable/chunks/VUzF8pDT.js
+++
@@ -1,1 +0,0 @@
-import{F as a,G as l}from"./D6YVsugE.js";function f(s,n,r){var t=s==null?"":""+s;return t===""?null:t}function p(s,n,r,t,u,c){var i=s[a];if(l||i!==r||i===void 0){var e=f(r);(!l||e!==s.getAttribute("class"))&&(e==null?s.removeAttribute("class"):s.className=e),s[a]=r}return c}export{p as s};
--- .svelte-kit/output/server/nodes/4.js
+++
@@ -1,6 +0,0 @@
-
-
-export const index = 4;
-export const imports = ["_app/immutable/nodes/4.txozFYLU.js","_app/immutable/chunks/Bzak7iHL.js","_app/immutable/chunks/WCcQgqSG.js","_app/immutable/chunks/D6YVsugE.js","_app/immutable/chunks/CLNtX1Hv.js","_app/immutable/chunks/C21KFVfL.js","_app/immutable/chunks/Cai5QfDo.js","_app/immutable/chunks/Cveq1ROl.js","_app/immutable/chunks/BUR0yJqe.js"];
-export const stylesheets = [];
-export const fonts = [];
